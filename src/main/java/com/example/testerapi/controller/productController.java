bcd0cdac22af4ff78312b0e270fe7b1e02565a46
knikhil52 <kesepattapu.nikhil@gmail.com>
2025-09-02 21:52:35 +0530

    code organisation

--- src/main/java/com/example/testerapi/controller/productController.java
+++ src/main/java/com/example/testerapi/controller/productController.java
@@ -1,19 +1,13 @@
 package com.example.testerapi.controller;
 
-import com.example.testerapi.model.Instrument;
 import com.example.testerapi.model.Product;
 import com.example.testerapi.service.productService;
-import io.micrometer.core.ipc.http.HttpSender;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 
 @RestController
@@ -32,28 +26,6 @@ public class productController {
 
     @PutMapping
     public ResponseEntity<String> putProduct(@RequestBody Product product){
-        Optional<Product> productDTO=prodService.findProduct(product);
-        if(productDTO.isPresent()){
-            Product product_dto=productDTO.get();
-            product_dto.setSymbol(product.getSymbol());
-            List<Instrument> existing_instruments=product_dto.getInstruments();
-            Set<Integer> existUniqInstID=product_dto.getInstruments().stream().map(Instrument::getUniqInstrumentId).collect(Collectors.toSet());
-
-            product.getInstruments().forEach(instrument -> {
-                if(!existUniqInstID.contains(instrument.getUniqInstrumentId())){
-                    instrument.setProduct(product);
-                    existing_instruments.add(instrument);
-                }
-            });
-            product_dto.setInstruments(existing_instruments);
-            //System.out.println(product_dto);
-            prodService.addProduct(product_dto);
-            return new ResponseEntity<>("Product updated",HttpStatus.CREATED);
+        return new ResponseEntity<>(prodService.productHandler(product),HttpStatus.CREATED);
         }
-
-        else {
-            Product ret = prodService.addProduct(product);
-            return ResponseEntity.status(HttpStatus.CREATED).body("Product created");
-        }
-    }
 }
--- src/main/java/com/example/testerapi/service/productService.java
+++ src/main/java/com/example/testerapi/service/productService.java
@@ -6,22 +6,52 @@ import com.example.testerapi.repository.productRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class productService {
     @Autowired
     productRepository prodRepository;
 
-    public Product addProduct(Product product){
+    public Product addProduct(Product product) {
         if (product.getInstruments() != null && !product.getInstruments().isEmpty()) {
             for (Instrument instrument : product.getInstruments()) {
-                instrument.setProduct(product); // 🔥 This is the fix
+                instrument.setProduct(product);
             }
         }
-        return(prodRepository.save(product));
+        return (prodRepository.save(product));
     }
-    public Optional<Product> findProduct(Product product){
+
+    public Optional<Product> findProduct(Product product) {
         return prodRepository.findByUniqProdID(product.getUniqProdID());
+    }
+
+    public void productUpdate(Product product_dto, Product product) {
+        product_dto.setSymbol(product.getSymbol());
+        List<Instrument> existing_instruments = product_dto.getInstruments();
+        Set<Integer> existUniqInstID = product_dto.getInstruments().stream().map(Instrument::getUniqInstrumentId).collect(Collectors.toSet());
+
+        product.getInstruments().forEach(instrument -> {
+            if (!existUniqInstID.contains(instrument.getUniqInstrumentId())) {
+                existing_instruments.add(instrument);
+            }
+        });
+        product_dto.setInstruments(existing_instruments);
+        this.addProduct(product_dto);
+    }
+
+    public String productHandler(Product product) {
+        Optional<Product> productDTO = this.findProduct(product);
+        if (productDTO.isPresent()) {
+                this.productUpdate(productDTO.get(),product);
+                return "Product updated";
+        }
+        else {
+                this.addProduct(product);
+                return "Product created";
+        }
     }
 }
